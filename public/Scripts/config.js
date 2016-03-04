2f80f3b29c0e3d8ea228bd9724f0ab090f0d7ab5
ArturNizamutdinov <nizamutdin.art@yandex.ru>
2016-03-04 16:55:41 +0300

    Added and configured angular-google-maps

---
+++ public/Scripts/config.js
@@ -0,0 +1,7 @@
+App.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyCPa8E_aUoZfd6eKS_-Yimbkggt2n48oQ8',
+        v: '3.22', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+});
--- server.js
+++ server.js
@@ -17,24 +17,24 @@ var clients = {};
 
 // WebSocket-сервер на порту 8081
 var webSocketServer = new WebSocketServer.Server({
-	port: 8081
+    port: 8081
 });
 webSocketServer.on('connection', function (ws) {
 
-	var id = Math.random();
-	clients[id] = ws;
-	console.log("новое соединение " + id);
+    var id = Math.random();
+    clients[id] = ws;
+    console.log("новое WS соединение " + id);
 
-	ws.on('message', function (message) {
-		console.log('получено сообщение ' + message);
+    ws.on('message', function (message) {
+        console.log('получено WS сообщение ' + message);
 
         sendOutWS(message);
-	});
+    });
 
-	ws.on('close', function () {
-		console.log('соединение закрыто ' + id);
-		delete clients[id];
-	});
+    ws.on('close', function () {
+        console.log('соединение WS закрыто ' + id);
+        delete clients[id];
+    });
 
 });
 
@@ -55,78 +55,67 @@ function sendOutCOMET(message) {
 //======================== COMET===================
 var http = require('http');
 var url = require('url');
-var querystring = require('querystring');
-//var static = require('node-static');
-
-//var fileServer = new static.Server('.');
-
 var subscribers = {};
 
 function onSubscribe(req, res) {
-	var id = Math.random();
+    var id = Math.random();
 
-	res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-	res.setHeader("Cache-Control", "no-cache, must-revalidate");
+    res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+    res.setHeader("Cache-Control", "no-cache, must-revalidate");
 
-	subscribers[id] = res;
-	console.log("новый клиент " + id + ", клиентов:" + Object.keys(subscribers).length);
+    subscribers[id] = res;
+    console.log("новый XHR клиент " + id + ", клиентов:" + Object.keys(subscribers).length);
 
-	req.on('close', function () {
-		delete subscribers[id];
-		console.log("клиент "+id+" отсоединился, клиентов:" + Object.keys(subscribers).length);
-	});
+    req.on('close', function () {
+        delete subscribers[id];
+        console.log("XHR клиент " + id + " отсоединился, клиентов:" + Object.keys(subscribers).length);
+    });
 
 }
 
 function publish(message) {
 
-	console.log("есть сообщение, клиентов:" + Object.keys(subscribers).length);
+    console.log("есть сообщение, клиентов:" + Object.keys(subscribers).length);
 
     sendOutCOMET(message);
 
-	subscribers = {};
+    subscribers = {};
 }
 
 function accept(req, res) {
-	var urlParsed = url.parse(req.url, true);
+    var urlParsed = url.parse(req.url, true);
 
-	// новый клиент хочет получать сообщения
-	//if (urlParsed.pathname == '/publishsubscribe/') {
-		if (urlParsed.pathname == '/subscribe') {
-		onSubscribe(req, res); // собственно, подписка
-		return;
-	}
+    // новый клиент хочет получать сообщения
+    if (urlParsed.pathname == '/subscribe') {
+        onSubscribe(req, res); // собственно, подписка
+        return;
+    }
 
-	console.log('XHR: '+req);
-	console.log('XHR urlParsed.pathname '+urlParsed.pathname);
+    console.log('XHR: ' + req);
+    console.log('XHR urlParsed.pathname ' + urlParsed.pathname);
 
-	// отправка сообщения
-	//if (urlParsed.pathname == '/publishpublish/' && req.method == 'POST') {
-		if (urlParsed.pathname == '/publish' && req.method == 'POST') {
-		// принять POST-запрос
-		req.setEncoding('utf8');
-		var message = '';
-		req.on('data', function (chunk) {
-			message += chunk;
-		}).on('end', function () {
-			publish(message); // собственно, отправка
-			res.end("ok");
-		});
+    // отправка сообщения
+    if (urlParsed.pathname == '/publish' && req.method == 'POST') {
+        // принять POST-запрос
+        req.setEncoding('utf8');
+        var message = '';
+        req.on('data', function (chunk) {
+            message += chunk;
+        }).on('end', function () {
+            publish(message); // собственно, отправка
+            res.end("ok");
+        });
 
-		return;
-	}
-
-	// всё остальное -- статика
-	//fileServer.serve(req, res);
-
+        return;
+    }
 }
 
 
 // -----------------------------------
 
 if (!module.parent) {
-	http.createServer(accept).listen(8082);
-	console.log('Сервер запущен на порту 8082');
+    http.createServer(accept).listen(8082);
+    console.log('Сервер запущен на порту 8082');
 } else {
-	exports.accept = accept;
+    exports.accept = accept;
 }
