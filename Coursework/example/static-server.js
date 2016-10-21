bc15173275b089c3e96ee101ba99a9660955efa6
ArturNizamutdinov <nizamutdin.art@yandex.ru>
2016-10-21 21:59:59 +0300

    Removed useless Coursework folder

--- Coursework/example/static-server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express'),
-    app = express(),
-    __dirname = './public';
-
-app.use(express.static(__dirname));
-console.info('\n\nStatic server started! Port 8088!');
-app.listen(8088);
